d70f7b252c5a38d1aeae110b4acd78b1741cb331
flynnGodslayer <victor_avila_felipe@hotmail.com>
2018-05-24 11:35:12 -0500

    Proyecto inicial paskinel
    Primera linea base del proyecto

---
+++ JS/generausuario.js
@@ -0,0 +1,6 @@
+    
+       function copiar()
+		{
+			document.getElementById("Usuario").value=document.getElementById("apellidos").value + "_" + document.getElementById("nombre").value ;
+		} 
+    
