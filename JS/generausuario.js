ff991e2f497ad8d7ce78220b3492f95b47fb0326
flynnGodslayer <victor_avila_felipe@hotmail.com>
2018-05-29 09:16:23 -0500

    Delete generausuario.js

--- JS/generausuario.js
+++
@@ -1,6 +0,0 @@
-    
-       function copiar()
-		{
-			document.getElementById("Usuario").value=document.getElementById("apellidos").value + "_" + document.getElementById("nombre").value ;
-		} 
-    
