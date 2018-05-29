92f59072f059b8f12a33a223c2f317fdf739ca19
flynnGodslayer <victor_avila_felipe@hotmail.com>
2018-05-29 09:17:36 -0500

    Delete mensajes.js

--- JS/mensajes.js
+++
@@ -1,9 +0,0 @@
-jQuery.extend(jQuery.validator.messages, {
-    required: "campo obligatorio", 
-    digits:"por favor escriba solo digitos",
-    email: "Por favor, escribe una dirección de correo válida",
-    maxlength: jQuery.validator.format("Por favor, no escribas más de {0} caracteres.")
-    
-    
-    
-});
